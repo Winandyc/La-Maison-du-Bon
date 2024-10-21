a29cfa2cd80b1bd4a15792ef7563f3e16d33066b
Winandyc <winandychristopher@gmail.com>
2024-10-21 19:24:31 +0200

    Add basename to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './modules/Home';
 import About from './modules/About';
 import Rooms from './modules/Rooms';
-// import Reviews from './modules/Reviews';
 import Menu from './modules/Menu';
 import { PizzasMenu } from './components/PizzasMenu';
-// import RestaurantMenu from './components/RestaurantMenu';
 import NotFound from './modules/NotFound';
 import LegalesNotices from './components/LegalesNotices';
 import Footer from './components/Footer';
@@ -15,15 +13,13 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/La-Maison-du-Bon">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/menu/pizzas" element={<PizzasMenu />} />
-          {/* <Route path="/menu/restaurant" element={<RestaurantMenu />} /> */}
           <Route path="/rooms" element={<Rooms />} />
-          {/* <Route path="/reviews" element={<Reviews />} /> */}
           <Route path="/about" element={<About />} />
           <Route path="/LegalesNotices" element={<LegalesNotices />} />
           <Route path="*" element={<NotFound />} />
